4abc38d5ace1ba1336bfe87239e725eef00c1d75
ctninman <89664157+ctninman@users.noreply.github.com>
2022-06-27 11:56:12 -0400

    removed start code

---
+++ src/pages/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function NavBar(props) {
+	return (
+		<div>
+			<div>
+				<h3>About</h3>
+			</div>
+			<div>
+				<h3>Projects</h3>
+			</div>
+			<div>
+				<h3>Blog</h3>
+			</div>
+			<div>
+				<h3>Skills</h3>
+			</div>
+			<div>
+				<h3>Contact</h3>
+			</div>
+		</div>
+	);
+}
+
+export default NavBar;
