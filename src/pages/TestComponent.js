5d0f1181c34fe3adbe8c422043a62e3bb5cfa070
ctninman <89664157+ctninman@users.noreply.github.com>
2022-06-26 21:44:31 -0400

    first update

---
+++ src/pages/TestComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TestComponent(props) {
+	return (
+		<div>
+			<h1>Dude, you just be like... um. Gog to bed</h1>
+		</div>
+	);
+}
+
+export default TestComponent;
