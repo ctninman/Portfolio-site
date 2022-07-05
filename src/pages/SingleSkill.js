e72d8b9804e56d072d39f4cefb92157d66069306
ctninman <89664157+ctninman@users.noreply.github.com>
2022-07-05 15:28:46 -0400

    removed document call for vh and vw 2

--- src/pages/SingleSkill.js
+++ src/pages/SingleSkill.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 function SingleSkill({skill}) {
 
-	const vw = Math.max((document.documentElement.clientWidth || 0, window.innerWidth || 0) *.8)
-	const vh = Math.max((document.documentElement.clientHeight || 0, window.innerHeight || 0) *.8)
 
 	const skillAnimationClasses = ['sk1', 'sk2', 'sk3', 'sk4', 'sk5']
 	const randomClassName = skillAnimationClasses[Math.floor(Math.random()* skillAnimationClasses.length)] 
