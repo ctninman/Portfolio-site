d298bf71d9b7fc22892db06bcb97d40d92dac4c9
ctninman <89664157+ctninman@users.noreply.github.com>
2022-07-05 10:50:35 -0400

    test remove project variable2

--- src/pages/Tools.js
+++ src/pages/Tools.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Tools({displayedProject}) {
 	return (
 		<div>
-			<h2>GOAL: {displayedProject.goal}</h2>
+			{/* <h2>GOAL: {displayedProject.goal}</h2> */}
+			
 			<ul>
 				{/* {displayedProject.techPoints.map(point => (
 					<li key={point}>{point}</li>
